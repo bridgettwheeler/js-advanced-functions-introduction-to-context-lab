a7001dd93a18a95ba138ed971a32de6e8512d4b4
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-08-04 11:35:24 -0400

    got help here

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function createEmployeeRecord([firstName, familyName, title, payRate]) {
-    let recordInput = [firstName, familyName, title, payRate]
-    const newEngineers = map(recordInput, function(element) {
-        return Object.assign({}, element, { "first name: "`$firstName`, "family name: " `$familyName` });
-      });
+function createEmployeeRecord(record) {
+    
+    return {
+        firstName: record[0],
+        familyName: record[1],
+        title: record[2],
+        payPerHour: record[3],
+        timeInEvents: [],
+        timeOutEvents: []
 
     }
 
 }
+
+let createEmployeeRecords = createEmployeeRecord.map(obj => {
+    let rObj = {}
+    rObj[obj.key] = obj.value
+    return rObj
