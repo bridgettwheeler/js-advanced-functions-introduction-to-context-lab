36fc91188ae6ef08f26a82b0ae7db21f367cd0cb
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-08-04 21:20:07 -0400

    stopping point

--- index.js
+++ index.js
@@ -15,7 +15,24 @@ function createEmployeeRecords(records) {
     return records.map(record => createEmployeeRecord(record))
 }
 function createTimeInEvent(record, dateStamp) {
-    let dateAndTime =dateStamp.split(" ")
-    return Object.assign(record.timeInEvents, {type: "TimeIn", hour: `$dateStamp.charAt(11,12)`, date: dateAndTime[0]})
+    let dateAndTime = dateStamp.split(' ')
+    console.log(dateAndTime)
+    record.timeInEvents.push({type: "TimeIn", hour: parseInt(dateAndTime[1]), date: dateAndTime[0]})
+    return record
+}
+function createTimeOutEvent(record, dateStamp) {
+    let dateAndTime = dateStamp.split(' ')
+    record.timeOutEvents.push({type: "TimeOut", hour: parseInt(dateAndTime[1]), date: dateAndTime[0]})
+    return record
+}
+//find a record who's date is equal to date in argument. Go in the object that has the key value pair tyoe:TimeOut 
+//and subtract it from the object that has the key value pair type: timeIn
+
+function hoursWorkedOnDate(record, date) {
+    console.log("record:",record)
+    const timeIn = record.timeInEvents.filter(event => event.date === date)
+    const timeOut = record.timeOutEvents.filter(event => event.date === date)
+    console.log("Bridgett, this is timeIn:", timeIn)
+    return (timeOut[0].hour - timeIn[0].hour)/100
 
 }
